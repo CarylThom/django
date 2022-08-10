f4f3a4eba616345738bfe89f10681627a274c507
CarylThom <cat.twnti@gmail.com>
2022-08-10 13:39:13 +0000

    Changes to .gitignore

---
+++ env.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "")
+os.environ.setdefault("DEBUG", "True")
+os.environ.setdefault("DEVELOPMENT", "True")
+os.environ.setdefault("DB_URL", "")
+os.environ.setdefault("DATABASE_URL", "")
